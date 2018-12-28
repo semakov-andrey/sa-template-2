255d5da57f7cfbc99e3ecaef132f35499b575887
Семаков Андрей <asemakov@htc-cs.ru>
2018-12-28 18:13:01 +0400

    commit

---
+++ install.js
@@ -0,0 +1,68 @@
+'use strict';
+
+const project         = '../../';
+const fs              = require('fs');
+const path            = require('path');
+const ncp             = require('ncp');
+
+if(path.basename(path.resolve(__dirname, '../')) !== 'node_modules') return;
+
+let templateJSON      = require('./package.json');
+let packageJSON       = require(`${project}package.json`);
+
+/* update readme */
+if(!fs.existsSync(path.resolve(__dirname, project, 'readme.md'))) {
+  ncp.ncp(path.resolve(__dirname, 'readme.md'), path.resolve(__dirname, project, 'readme.md'), error => error ? console.error('\x1b[31m%s\x1b[0m', 'Error: ' + error) : console.log('Success: readme updated'));
+}
+
+/* update compiler scripts */
+ncp.ncp(path.resolve(__dirname, 'start.js'), path.resolve(__dirname, project, 'start.js'), error => error ? console.error('\x1b[31m%s\x1b[0m', 'Error: ' + error) : console.log('Success: start.js updated'));
+ncp.ncp(path.resolve(__dirname, 'start.js'), path.resolve(__dirname, project, 'start.js'), error => error ? console.error('\x1b[31m%s\x1b[0m', 'Error: ' + error) : console.log('Success: build.js updated'));
+
+/* update webpack configuration */
+ncp.ncp(path.resolve(__dirname, 'config'), path.resolve(__dirname, project, 'config'), error => error ? console.error('\x1b[31m%s\x1b[0m', 'Error: ' + error) : console.log('Success: webpack configuration updated'));
+
+/* update package.json */
+delete templateJSON.dependencies['ncp'];
+let scripts = { 
+  start: 'node start.js',
+  build: 'node build.js',
+  module: 'node node_modules/sa-template-1/module.js'
+};
+let json = {
+  ...packageJSON,
+  scripts: {
+    ...packageJSON.scripts,
+    ...scripts
+  },
+  devDependencies: {
+    ...packageJSON.devDependencies,
+    ...templateJSON.dependencies
+  },
+  config: {
+    entries: templateJSON.config.entries,
+    browsers: templateJSON.config.browsers,
+    ...packageJSON.config,
+    directories: {
+      ...templateJSON.config.directories,
+      ...(packageJSON.config && packageJSON.config.directories ? packageJSON.config.directories : {}),
+      files: {
+        ...templateJSON.config.directories.files,
+        ...(packageJSON.config && packageJSON.config.directories && packageJSON.config.directories.files ? packageJSON.config.directories.files : {})
+      }
+    }
+  }
+};
+fs.writeFile(path.resolve(__dirname, project, 'package.json'), JSON.stringify(json, null, 2), 'utf8', error => error ? console.error('\x1b[31m%s\x1b[0m', 'Error: ' + error) : console.log('Success: package.json updated'));
+
+/* update gitignore */
+let gitignore = ['node_modules', 'build', '.vscode', '*.log', 'Thumbs.db', '.idea', '.grunt', '.DS_Store', 'bash.exe.stackdump', '.editorconfig', '.yo-rc.json'];
+let writeGitIgnore = (data, newData = []) => {
+  fs.writeFile(path.resolve(__dirname, project, '.gitignore'), [...new Set([...data, ...newData])].join('\r\n'), 'utf8', error => error ? console.error('\x1b[31m%s\x1b[0m', 'Error: ' + error) : console.log('Success: gitignore updated'));
+};
+if(fs.existsSync(path.resolve(__dirname, project, '.gitignore'))) {
+  fs.readFile(path.resolve(__dirname, project, '.gitignore'), 'utf8', (error, data) => {
+    if(error) return console.error('\x1b[31m%s\x1b[0m', 'Error: ' + error);
+    writeGitIgnore(data.split('\r\n'), gitignore);
+  });
+} else writeGitIgnore(gitignore);
---
+++ module.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const fs              = require('fs');
+const path            = require('path');
+const installed       = path.basename(path.resolve(__dirname, '../')) === 'node_modules';
+const project         = installed ? '../../' : './';
+
+let newModule         = process.argv[2] && typeof(process.argv[2]) === 'string' ? process.argv[2] : '';
+
+String.prototype.capitalize = function() {
+  return this.charAt(0).toUpperCase() + this.slice(1);
+};
+
+if(newModule !== '') {
+  if(fs.existsSync(path.resolve(__dirname, project, 'src', 'modules'))) {
+    if(!fs.existsSync(path.resolve(__dirname, project, 'src', 'modules', newModule))) {
+      fs.mkdirSync(path.resolve(__dirname, project, 'src', 'modules', newModule));
+      fs.writeFile(path.resolve(__dirname, project, 'src', 'modules', newModule, `${newModule}.pug`), `mixin ${newModule}\r\n\t.${newModule}`, 'utf8', error => {
+        if(error) return console.error('\x1b[31m%s\x1b[0m', 'Error: ' + error);
+        console.log('Success: .pug file created');
+      });
+      fs.writeFile(path.resolve(__dirname, project, 'src', 'modules', newModule, `${newModule}.scss`), `.${newModule} {\r\n\r\n\}`, 'utf8', error => {
+        if(error) return console.error('\x1b[31m%s\x1b[0m', 'Error: ' + error);
+        console.log('Success: .scss file created');
+      });
+      fs.writeFile(path.resolve(__dirname, project, 'src', 'modules', newModule, `${newModule}.js`), `'use strict';\r\n\r\nexport default class ${newModule.capitalize()} {\r\n\tconstructor() {\r\n\t\t\r\n\t}\r\n}`, 'utf8', error => {
+        if(error) return console.error('\x1b[31m%s\x1b[0m', 'Error: ' + error);
+        console.log('Success: .js file created');
+      });
+    } else console.error('\x1b[31m%s\x1b[0m', 'Error: module exist already');
+  } else console.error('\x1b[31m%s\x1b[0m', 'Error: modules folder not found');
+} else console.error('\x1b[31m%s\x1b[0m', 'Error: enter name of module');
