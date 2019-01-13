f6e7e3de4dfb2c1e0c310ca9be4b2c1da234dc4c
semakov_andrey <semakov.andrey.web@yandex.ru>
2019-01-13 09:18:19 +0400

    commit

--- install.js
+++ install.js
@@ -12,18 +12,20 @@ const packageJSON               = require(`${project}package.json`);
 
 /* update readme */
 if(!fs.existsSync(path.resolve(__dirname, project, 'readme.md'))) {
-  ncp.ncp(path.resolve(__dirname, 'readme.md'), path.resolve(__dirname, project, 'readme.md'), error => error ? console.error('\x1b[31m%s\x1b[0m', 'Error: ' + error) : console.log('Success: readme updated'));
+  ncp.ncp(path.resolve(__dirname, 'readme.md'), path.resolve(__dirname, project, 'readme.md'), error => error ? console.error('\x1b[31m%s\x1b[0m', `Error:  ${error}`) : console.log('Success: readme updated'));
 }
 
 /* update compiler scripts */
-ncp.ncp(path.resolve(__dirname, 'start.js'), path.resolve(__dirname, project, 'start.js'), error => error ? console.error('\x1b[31m%s\x1b[0m', 'Error: ' + error) : console.log('Success: start.js updated'));
-ncp.ncp(path.resolve(__dirname, 'build.js'), path.resolve(__dirname, project, 'build.js'), error => error ? console.error('\x1b[31m%s\x1b[0m', 'Error: ' + error) : console.log('Success: build.js updated'));
+ncp.ncp(path.resolve(__dirname, 'start.js'), path.resolve(__dirname, project, 'start.js'), error => error ? console.error('\x1b[31m%s\x1b[0m', `Error:  ${error}`) : console.log('Success: start.js updated'));
+ncp.ncp(path.resolve(__dirname, 'build.js'), path.resolve(__dirname, project, 'build.js'), error => error ? console.error('\x1b[31m%s\x1b[0m', `Error:  ${error}`) : console.log('Success: build.js updated'));
 
 /* update stylelint */
-ncp.ncp(path.resolve(__dirname, '.stylelintrc'), path.resolve(__dirname, project, '.stylelintrc'), error => error ? console.error('\x1b[31m%s\x1b[0m', 'Error: ' + error) : console.log('Success: stylelint updated'));
+if(!fs.existsSync(path.resolve(__dirname, project, '.stylelintrc'))) {
+  ncp.ncp(path.resolve(__dirname, '.stylelintrc'), path.resolve(__dirname, project, '.stylelintrc'), error => error ? console.error('\x1b[31m%s\x1b[0m', `Error:  ${error}`) : console.log('Success: stylelint updated'));
+}
 
 /* update webpack configuration */
-ncp.ncp(path.resolve(__dirname, 'config'), path.resolve(__dirname, project, 'config'), error => error ? console.error('\x1b[31m%s\x1b[0m', 'Error: ' + error) : console.log('Success: webpack configuration updated'));
+ncp.ncp(path.resolve(__dirname, 'config'), path.resolve(__dirname, project, 'config'), error => error ? console.error('\x1b[31m%s\x1b[0m', `Error:  ${error}`) : console.log('Success: webpack configuration updated'));
 
 /* update package.json */
 delete templateJSON.dependencies['ncp'];
@@ -58,16 +60,16 @@ const json = {
     }
   }
 };
-fs.writeFile(path.resolve(__dirname, project, 'package.json'), JSON.stringify(json, null, 2), 'utf8', error => error ? console.error('\x1b[31m%s\x1b[0m', 'Error: ' + error) : console.log('Success: package.json updated'));
+fs.writeFile(path.resolve(__dirname, project, 'package.json'), JSON.stringify(json, null, 2), 'utf8', error => error ? console.error('\x1b[31m%s\x1b[0m', `Error:  ${error}`) : console.log('Success: package.json updated'));
 
 /* update gitignore */
 const gitignore = ['node_modules', 'build', '.vscode', '*.log', 'Thumbs.db', '.idea', '.grunt', '.DS_Store', 'bash.exe.stackdump', '.editorconfig', '.yo-rc.json'];
 const writeGitIgnore = (data, newData = []) => {
-  fs.writeFile(path.resolve(__dirname, project, '.gitignore'), [...new Set([...data, ...newData])].join('\r\n'), 'utf8', error => error ? console.error('\x1b[31m%s\x1b[0m', 'Error: ' + error) : console.log('Success: gitignore updated'));
+  fs.writeFile(path.resolve(__dirname, project, '.gitignore'), [...new Set([...data, ...newData])].join('\r\n'), 'utf8', error => error ? console.error('\x1b[31m%s\x1b[0m', `Error:  ${error}`) : console.log('Success: gitignore updated'));
 };
 if(fs.existsSync(path.resolve(__dirname, project, '.gitignore'))) {
   fs.readFile(path.resolve(__dirname, project, '.gitignore'), 'utf8', (error, data) => {
-    if(error) return console.error('\x1b[31m%s\x1b[0m', 'Error: ' + error);
+    if(error) return console.error('\x1b[31m%s\x1b[0m', `Error:  ${error}`);
     writeGitIgnore(data.split('\r\n'), gitignore);
   });
 } else writeGitIgnore(gitignore);
